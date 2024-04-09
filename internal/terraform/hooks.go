c32f1172de5b71f4ef3e118c04feb12f24890fe1
Alex McKinney <alexmckinney01@gmail.com>
2024-04-09 14:28:43 -0700

    Refactor provider with hooks.go

---
+++ internal/terraform/hooks.go
@@ -0,0 +1,54 @@
+package terraform
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/provider"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+)
+
+// Ensure Provider satisfies the terraform provider interface(s).
+var _ provider.Provider = (*Provider)(nil)
+
+// Provider defines the provider implementation.
+type Provider struct {
+	base *baseProvider
+
+	// version is set to the provider version on release, "dev" when the
+	// provider is built and ran locally, and "test" when running acceptance
+	// testing.
+	version string
+}
+
+// NewProvider returns a new vellum terraform provider.
+func NewProvider(version string) func() provider.Provider {
+	return func() provider.Provider {
+		return &Provider{
+			base:    newBaseProvider(version),
+			version: version,
+		}
+	}
+}
+
+// --- Override the following methods to customize the provider ---
+
+func (p *Provider) Metadata(ctx context.Context, req provider.MetadataRequest, resp *provider.MetadataResponse) {
+	p.base.Metadata(ctx, req, resp)
+}
+
+func (p *Provider) Schema(ctx context.Context, req provider.SchemaRequest, resp *provider.SchemaResponse) {
+	p.base.Schema(ctx, req, resp)
+}
+
+func (p *Provider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
+	p.base.Configure(ctx, req, resp)
+}
+
+func (p *Provider) Resources(ctx context.Context) []func() resource.Resource {
+	return p.base.Resources(ctx)
+}
+
+func (p *Provider) DataSources(ctx context.Context) []func() datasource.DataSource {
+	return p.base.DataSources(ctx)
+}
--- internal/terraform/provider.go
+++ internal/terraform/provider.go
@@ -2,54 +2,98 @@ package terraform
 
 import (
 	"context"
+	"os"
 
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/provider"
+	"github.com/hashicorp/terraform-plugin-framework/provider/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
-	"github.com/vellum-ai/terraform-provider-vellum/internal/terraform/base"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	vellumdatasource "github.com/vellum-ai/terraform-provider-vellum/internal/terraform/datasource"
+	vellumresource "github.com/vellum-ai/terraform-provider-vellum/internal/terraform/resource"
+	vellumclient "github.com/vellum-ai/terraform-provider-vellum/internal/vellum/client"
+	"github.com/vellum-ai/terraform-provider-vellum/internal/vellum/option"
 )
 
-// Ensure Provider satisfies the terraform provider interface(s).
-var _ provider.Provider = (*Provider)(nil)
+// Ensure baseProvider satisfies the terraform provider interface(s).
+var _ provider.Provider = (*baseProvider)(nil)
 
-// Provider defines the provider implementation.
-type Provider struct {
-	base *base.Provider
+// ProviderModel describes the provider data model.
+type ProviderModel struct {
+	ApiKey types.String `tfsdk:"api_key"`
+}
 
+// basebaseProvider defines the base provider implementation.
+type baseProvider struct {
 	// version is set to the provider version on release, "dev" when the
 	// provider is built and ran locally, and "test" when running acceptance
 	// testing.
 	version string
 }
 
-// NewProvider returns a new vellum terraform provider.
-func NewProvider(version string) func() provider.Provider {
-	return func() provider.Provider {
-		return &Provider{
-			base:    base.NewProvider(version).(*base.Provider),
-			version: version,
-		}
+// newBaseProvider returns a new vellum terraform provider.
+func newBaseProvider(version string) *baseProvider {
+	return &baseProvider{
+		version: version,
 	}
 }
 
-// ---
-
-func (p *Provider) Metadata(ctx context.Context, req provider.MetadataRequest, resp *provider.MetadataResponse) {
-	p.base.Metadata(ctx, req, resp)
+func (b *baseProvider) Metadata(ctx context.Context, req provider.MetadataRequest, resp *provider.MetadataResponse) {
+	resp.TypeName = "vellum"
+	resp.Version = b.version
 }
 
-func (p *Provider) Schema(ctx context.Context, req provider.SchemaRequest, resp *provider.SchemaResponse) {
-	p.base.Schema(ctx, req, resp)
+func (b *baseProvider) Schema(ctx context.Context, req provider.SchemaRequest, resp *provider.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		Attributes: map[string]schema.Attribute{
+			"api_key": schema.StringAttribute{
+				MarkdownDescription: "API Key to authenticate with the Vellum API",
+				Optional:            true,
+			},
+		},
+	}
 }
 
-func (p *Provider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
-	p.base.Configure(ctx, req, resp)
+func (b *baseProvider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
+	var model ProviderModel
+
+	resp.Diagnostics.Append(req.Config.Get(ctx, &model)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	apiKey := os.Getenv("VELLUM_API_KEY")
+	if !model.ApiKey.IsNull() {
+		apiKey = model.ApiKey.ValueString()
+	}
+
+	if apiKey == "" {
+		resp.Diagnostics.AddAttributeError(
+			path.Root("api_key"),
+			"An API key is required to use the vellum provider",
+			"You must set a VELLUM_API_KEY or specify an api_key in the provider constructor",
+		)
+		return
+	}
+
+	client := vellumclient.NewClient(
+		option.WithApiKey(
+			apiKey,
+		),
+	)
+	resp.DataSourceData = client
+	resp.ResourceData = client
 }
 
-func (p *Provider) Resources(ctx context.Context) []func() resource.Resource {
-	return p.base.Resources(ctx)
+func (b *baseProvider) Resources(ctx context.Context) []func() resource.Resource {
+	return []func() resource.Resource{
+		vellumresource.NewDocumentIndex,
+	}
 }
 
-func (p *Provider) DataSources(ctx context.Context) []func() datasource.DataSource {
-	return p.base.DataSources(ctx)
+func (b *baseProvider) DataSources(ctx context.Context) []func() datasource.DataSource {
+	return []func() datasource.DataSource{
+		vellumdatasource.NewDocumentIndex,
+	}
 }
